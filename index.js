e72cdff9e1388c5635941b4707b114e311523f78
Ryan Allen <ryan.c.allen.work@gmail.com>
2021-10-18 21:51:28 -0700

    added index.js questions and functions

--- index.js
+++ index.js
@@ -1,2 +1,162 @@
 const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const generateHTML = require("./util/generateHtml");
 
+const team = [];
+
+function userQuestions() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Select type of team member to add.",
+        name: "selections",
+        choices: [
+          "Add Intern",
+          "Add Manager",
+          "Add Engineer",
+          "No team members left to add",
+        ],
+      },
+    ])
+    .then((ans) => {
+      switch (ans.selections) {
+        case "Add Intern":
+          console.log("Intern added!");
+          addIntern();
+          break;
+
+        case "Add Manager":
+          console.log("Manager added!");
+          addManager();
+          break;
+
+        case "Add Engineer":
+          console.log("Engineer added!");
+          addEngineer();
+          break;
+
+        case "No team members left to add":
+          console.log("Team building complete!");
+          console.log(`${team}`);
+          console.log(team[0]);
+          team.forEach((member) => {
+            console.log(member.role + " role");
+          });
+          generateHTML();
+          break;
+
+        default:
+          console.log("See ya!");
+          break;
+      }
+    });
+}
+
+function addManager() {
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your employee ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your office number?",
+        name: "number",
+      },
+    ])
+    .then(({ name, id, email, number }) => {
+      console.log(name);
+      const man = new Manager(name, id, email, number);
+      team.push(man);
+      console.log(team);
+      console.log(team.role);
+      userQuestions();
+    });
+}
+
+function addEngineer() {
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your employee ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your office number?",
+        name: "number",
+      },
+    ])
+    .then(({ name, id, email, number }) => {
+      console.log(name);
+      const eng = new Engineer(name, id, email, number);
+      team.push(eng);
+      console.log(team);
+      console.log(team.role);
+      userQuestions();
+    });
+}
+
+function addIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your employee ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Which school did you graduate from?",
+        name: "school",
+      },
+    ])
+    .then(({ name, id, email, school }) => {
+      console.log(name);
+      const int = new Intern(name, id, email, school);
+      team.push(int);
+      console.log(team);
+      console.log(team.role);
+      userQuestions();
+    });
+}
+
+
+fs.writeFile(`index(${member}).html`, generateHTML);
