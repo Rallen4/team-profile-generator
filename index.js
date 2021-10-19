6e6a82c810d4e285f0645eb96a028d5926675545
Ryan Allen <ryan.c.allen.work@gmail.com>
2021-10-18 23:52:39 -0700

    completed assignment with help of tutor

--- index.js
+++ index.js
@@ -4,57 +4,61 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const generateHTML = require("./util/generateHtml");
+const fs = require("fs");
+const path = require("path");
+const outputDirectory = path.resolve(__dirname, "output");
+const outputPath = path.join(outputDirectory, "team.html");
 
 const team = [];
 
-function userQuestions() {
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        message: "Select type of team member to add.",
-        name: "selections",
-        choices: [
-          "Add Intern",
-          "Add Manager",
-          "Add Engineer",
-          "No team members left to add",
-        ],
-      },
-    ])
-    .then((ans) => {
-      switch (ans.selections) {
-        case "Add Intern":
-          console.log("Intern added!");
-          addIntern();
-          break;
+// function userQuestions() {
+//   inquirer
+//     .prompt([
+//       {
+//         type: "list",
+//         message: "Select type of team member to add.",
+//         name: "selections",
+//         choices: [
+//           "Add Intern",
+//           "Add Manager",
+//           "Add Engineer",
+//           "No team members left to add",
+//         ],
+//       },
+//     ])
+//     .then((ans) => {
+//       switch (ans.selections) {
+//         case "Add Intern":
+//           console.log("Intern added!");
+//           addIntern();
+//           break;
 
-        case "Add Manager":
-          console.log("Manager added!");
-          addManager();
-          break;
+//         case "Add Manager":
+//           console.log("Manager added!");
+//           addManager();
+//           break;
 
-        case "Add Engineer":
-          console.log("Engineer added!");
-          addEngineer();
-          break;
+//         case "Add Engineer":
+//           console.log("Engineer added!");
+//           addEngineer();
+//           break;
 
-        case "No team members left to add":
-          console.log("Team building complete!");
-          console.log(`${team}`);
-          console.log(team[0]);
-          team.forEach((member) => {
-            console.log(member.role + " role");
-          });
-          generateHTML();
-          break;
+//         case "No team members left to add":
+//           console.log("Team building complete!");
+//           console.log(`${team}`);
+//           console.log(team[0]);
+//           // team.forEach((team) => {
+//             console.log(team.role + " role");
+//           });
+//           generateHTML();
+//           break;
 
-        default:
-          console.log("See ya!");
-          break;
-      }
-    });
-}
+//         default:
+//           console.log("See ya!");
+//           break;
+//       }
+//     });
+// }
 
 function addManager() {
     inquirer
@@ -77,12 +81,12 @@ function addManager() {
       {
         type: "input",
         message: "What is your office number?",
-        name: "number",
+        name: "officeNumber",
       },
     ])
-    .then(({ name, id, email, number }) => {
+    .then(({ name, id, email, officeNumber }) => {
       console.log(name);
-      const man = new Manager(name, id, email, number);
+      const man = new Manager(name, id, email, officeNumber);
       team.push(man);
       console.log(team);
       console.log(team.role);
@@ -111,12 +115,12 @@ function addEngineer() {
       {
         type: "input",
         message: "What is your office number?",
-        name: "number",
+        name: "officeNumber",
       },
     ])
-    .then(({ name, id, email, number }) => {
+    .then(({ name, id, email, officeNumber }) => {
       console.log(name);
-      const eng = new Engineer(name, id, email, number);
+      const eng = new Engineer(name, id, email, officeNumber);
       team.push(eng);
       console.log(team);
       console.log(team.role);
@@ -158,5 +162,44 @@ function addIntern() {
     });
 }
 
+function userQuestions() {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "Who would you like to add to your team?",
+      choices: ["Engineer", "Intern", "Manager", "Nobody else"],
+      name: "build"
+    }
 
-fs.writeFile(`index(${member}).html`, generateHTML);
+  ]).then(ans=> {
+    switch(ans.build) {
+      case "Manager":
+      addManager();
+      break;
+
+      case "Engineer":
+      addEngineer();
+      break;
+
+      case "Intern":
+      addIntern();
+      break;
+
+      default:
+      buildTeam();
+      break;
+    }
+
+  })
+    
+}
+
+function buildTeam() {
+  console.log(team);
+  fs.writeFileSync(outputPath, generateHTML(team), "utf-8")
+}
+
+userQuestions();
+// .then(ans)
+// fs.writeFile(`index(${team}).html`, generateHTML);
+
